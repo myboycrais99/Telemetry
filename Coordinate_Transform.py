27a639c7f3208f0f668bd1bf595bc82322ca1e6b
myboycrais99 <rap4yd@gmail.com>
2016-10-17 22:14:09 -0600

    Added relA2B_ned function to Coordinate_Transform

--- Coordinate_Transform.py
+++ Coordinate_Transform.py
@@ -6,19 +6,18 @@ space for telemetry systems.
 from __future__ import division, print_function
 import numpy as np
 
+d2r = np.pi / 180
+r2d = 180 / np.pi
 
-def lla2ecef(lat, lon, alt, ellipse='WGS84'):
+def lla2ecef(LLA, ellipse='WGS84'):
     """Convert latitude, longitude, and altitude to earth-centered, earth-fixed
     (ECEF) cartesian
 
     Parameters
     ----------
-    lat : float
-        Geodetic latitude in radians
-    lon : float
-        Longitude in radians
-    alt: float
-        Altitude above reference ellipsoid in meters
+    LLA : float array
+        Geodetic latitude in radians, longitude in radians, and altitude above
+        reference ellipsoid in meters
     ellipse: str, optional
         Dictionary keyword to define which reference ellipsoid to use in
         calculations
@@ -32,6 +31,7 @@ def lla2ecef(lat, lon, alt, ellipse='WGS84'):
     ------
     None
     """
+    lat, lon, alt = LLA[0] * d2r, LLA[1] * d2r, LLA[2]
 
     ellipses = {
         'WGS84': {'a': 6378137, 'inv_f': 298.257223563, 'unit': 'm'}
@@ -52,16 +52,15 @@ def lla2ecef(lat, lon, alt, ellipse='WGS84'):
 
     return np.array([x, y, z], dtype=float)
 
-def lla2ten(lat, lon):
+def lla2ten(LLA):
     """Rotation matrix for converting from North-East-Down (NED) reference
     frame to Earth-Centered Earth-Fixed frame from Latitude and Longitude.
 
     Parameters
     ----------
-    lat : float
-        Geodetic latitude in radians
-    lon : float
-        Longitude in radians
+    LLA : float array
+        Geodetic latitude in radians, longitude in radians, and altitude above
+        reference ellipsoid in meters
 
     Returns
     -------
@@ -72,6 +71,7 @@ def lla2ten(lat, lon):
     ------
     None
     """
+    lat, lon = LLA[0] * d2r, LLA[1] * d2r
 
     ten = np.array([
         [-np.sin(lat) * np.cos(lon), -np.sin(lon), -np.cos(lat) * np.cos(lon)],
@@ -81,6 +81,39 @@ def lla2ten(lat, lon):
 
     return ten
 
+def relA2B_RAE(A, B):
+    """
+
+    Parameters
+    ----------
+    A : float array
+        LLA coordinates of an object
+    B : float array
+        LLA coordinates of an object
+
+    Return
+    ------
+    out : float array
+
+    Raises
+    ------
+    None
+    """
+
+    ecef1 = lla2ecef(A)
+    ecef2 = lla2ecef(B)
+
+    diff_ecef = ecef2 - ecef1
+    ten = lla2ten(A)
+
+    relA2B_ned = np.dot(ten, diff_ecef)
+
+    return np.array([
+        np.linalg.norm(relA2B_ned),
+        np.arctan2(relA2B_ned[1], relA2B_ned[0]) * r2d,
+        np.arctan2(-relA2B_ned[2],
+                   np.sqrt(relA2B_ned[0] ** 2 + relA2B_ned[1] ** 2)) * r2d
+    ])
 
 if __name__ == '__main__':
     pass
